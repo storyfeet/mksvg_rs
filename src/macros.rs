296fd211fbf28b7204719d2966e0a05364e946e6
storyfeet <thebeaky@psmail.net>
2019-09-30 17:43:28 +0100

    now parameters seem to work

--- src/macros.rs
+++ src/macros.rs
@@ -1,9 +1,32 @@
 //use crate::*;
+//head tail style propery list processing.
+#[macro_export]
+macro_rules! svg_properties {
+    //base case
+    ($e:expr,) => {
+        $e
+    };
+    //case with bracketed properties
+    ($e:expr, ($p:ident , ($($v:expr),*)),$($rest:tt)*) => (svg_properties!($e.$p($($v),*),$($rest)*));
+    //standard case
+    ($e:expr, ($p:ident , $v:expr) ,$($rest:tt)*) => (svg_properties!($e.$p($v),$($rest)*));
+}
 
 #[macro_export]
 macro_rules! svg_w {
-    ($wr:ident, $tp:ident $($k:tt = $v:tt)*)=> {
-        Tag::new(stringify!($tp))$(.$k($v))*.write(&mut $wr);
+    //simple svg component
+    ($wr:ident, ($nm:ident $($k:ident=$v:tt)*)) => {
+        svg_properties!(Tag::new(stringify!($nm)), $(($k,$v),)*).write(&mut $wr);
+    };
+    //wrapping svg component
+    ($wr:ident, ($nm:ident $($k:ident=$v:tt)* : $($child:tt)* ))=> {
+        let mut nw = svg_properties!(Tag::new(stringify!($nm)), $(($k,$v),),*).wrap(&mut $wr);
+        $(
+            svg_w!(nw , $child);
+        )*
+
+        drop(nw);
+
     };
 }
 
@@ -11,17 +34,54 @@ macro_rules! svg_w {
 mod test_macros {
     use super::*;
     use crate::*;
+
     #[test]
+    fn test_svg_properties() {
+        let mut s = String::new();
+        let mut fw = SvgFmt::new(&mut s);
+        svg_w!(fw,(rect w=5));
+        drop(fw);
+        assert_eq!(s, "<rect width=\"5\" />\n");
+    }
+
+    /*#[test]
     fn test_macro_does_a_thing() {
         let mut s = String::new();
         let mut fw = SvgFmt::new(&mut s);
-        svg_w! {
-            fw, rect x=32 y=0
-                
+        svg_w2! {
+            fw, (rect x=32 y=0)
         };
 
         drop(fw);
-        assert_eq!(s,"<help>");
+        assert_eq!(s, "<help>");
+    }*/
 
+    #[test]
+    fn test_wrapping_works() {
+        let mut s = String::new();
+        let mut fw = SvgFmt::new(&mut s);
+        svg_w! {fw,
+            ( g w=3:
+                (rect x=3 y=2)
+            )
+        }
+        drop(fw);
+        assert_eq!(s, "<g width=\"3\" >\n  <rect x=\"3\" y=\"2\" />\n</g>\n");
+    }
+
+    #[test]
+    fn test_multi_param_properties() {
+        let mut s = String::new();
+        let mut fw = SvgFmt::new(&mut s);
+        svg_w! {fw,
+            ( g translate=(3,4):
+                (rect x=3 y=2)
+            )
+        }
+        drop(fw);
+        assert_eq!(
+            s,
+            "<g transform=\"translate(3,4) \" >\n  <rect x=\"3\" y=\"2\" />\n</g>\n"
+        );
     }
 }
