b5129d7cdea07dd21577cca88c6b4f12b5e22cfe
storyfeet <thebeaky@psmail.net>
2019-09-30 22:24:57 +0100

    macros now handle loops and closures  TODO handle root svg and Text

--- src/macros.rs
+++ src/macros.rs
@@ -26,13 +26,32 @@ macro_rules! svg_w {
         )*
 
         drop(nw);
-
     };
+    ($wr:ident,( @if $b:expr => $($child:tt)*)) =>{
+        if $b {
+            $(
+                svg_w!($wr , $child);
+            )*
+        }
+    };
+    ($wr:ident, (@for $p:pat in $i:expr => $($child:tt)*)) =>{
+        for $p in $i  {
+            $(
+                svg_w!($wr , $child);
+            )*
+        }
+    };
+
+    ($wr:ident,(|$n:ident|  $e:tt))=> {
+        let f = |$n:&mut SvgWrite|$e;
+        f(&mut $wr);
+    }
+
 }
 
 #[cfg(test)]
 mod test_macros {
-    use super::*;
+    //    use super::*;
     use crate::*;
 
     #[test]
@@ -83,5 +102,53 @@ mod test_macros {
             s,
             "<g transform=\"translate(3,4) \" >\n  <rect x=\"3\" y=\"2\" />\n</g>\n"
         );
+    }
+
+    #[test]
+    fn test_deep_wrapping() {
+        let mut s = String::new();
+        let mut fw = SvgFmt::new(&mut s);
+        svg_w! {fw,
+            ( g translate=(3,4):
+                (g rotate=(15,24,45) :
+                    (ellipse x=5 y=2)
+                    (rect x=3 y=2)
+                )
+            )
+        }
+        drop(fw);
+        assert_eq!(
+            s,
+            "<g transform=\"translate(3,4) \" >\n  <g transform=\"rotate(15,24,45) \" >\n    <ellipse x=\"5\" y=\"2\" />\n    <rect x=\"3\" y=\"2\" />\n  </g>\n</g>\n"
+        );
+    }
+
+    #[test]
+    fn test_loop() {
+        let mut s = String::new();
+        let mut fw = SvgFmt::new(&mut s);
+        svg_w! {fw,
+            (@for n in 0..4 =>
+                (rect x=n)
+            )
+        }
+        drop(fw);
+        assert_eq!(
+            s,
+            "<rect x=\"0\" />\n<rect x=\"1\" />\n<rect x=\"2\" />\n<rect x=\"3\" />\n"
+        );
+    }
+
+    #[test]
+    fn test_macro_closure() {
+        let mut s = String::new();
+        let mut fw = SvgFmt::new(&mut s);
+        svg_w! {fw,
+            (text text_anchor="middle" :
+                (|v| {v.write("hello")})
+            )
+        }
+        drop(fw);
+        assert_eq!(s, "<text text-anchor=\"middle\" >\n  hello\n</text>\n");
     }
 }
